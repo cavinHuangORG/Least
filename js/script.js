0891619c80252e9fca935f344cd09f3619616555
任文龙 <yulanggong@gmail.com>
2015-07-03 15:09:51 +0800

    Add new features

--- js/script.js
+++ js/script.js
@@ -7,17 +7,16 @@ function tr(s) {
 
 var demoCSS;
 $(document).ready(function() {
-	$('.convert').click(parseInput);
-	$('#input').keyup(parseInput);
-	$('label.radio').click(parseInput);
+	$('#input, #variables').on('input propertychange', parseInput);
+	$('label.radio,label.checkbox').click(parseInput);
 
 	var demoURL='css/_demo.css';
-	//demoURL='../../theme/css/mo.css';
-	//demoURL='css/style.css';
 	$.get(demoURL, function(val) {
 		demoCSS=val;
 		$('.btn.demo').click(function() {
 			$('#input').val(demoCSS);
+			$('#variables').val('@red:#f00;\n@border:solid 1px #f00;\n//sass and stylus syntax are also supported');
+			parseInput();
 		});
 	});
 
@@ -31,6 +30,32 @@ var openingBracket;
 var closingBracket;
 var semiColumn;
 var eol;
+var keepOutsideComments;
+var keepinsideComments;
+var removeHacks;
+var vars = {};
+var comments = {};
+
+function trimColor(str){
+	return str.replace(/#([0-9a-f]{3}|[0-9a-f]{6})\b/ig, function (a) {
+		return a.toLowerCase()
+	})
+}
+
+function parseVars () {
+	vars = {};
+	variables = $('#variables').val().split(/[\n;]/);
+	$(variables).each(function(){
+		var pair = this.replace(/\/\*[\s\S]*?\*\//gm,'')
+		.replace(/\/\/[\s\S]*$/gm,'');
+
+		pair = pair.split(/[:=]/);
+		if(pair.length > 1){
+			vars[trimColor($.trim(pair[1]))] = $.trim(pair[0]);
+		}
+	})
+}
+
 function parseInput() {
 	var langVal=$('input:radio[name=lang]:checked').val();
 	if (langVal=='less') {
@@ -49,19 +74,64 @@ function parseInput() {
 		closingBracket='';
 		semiColumn='';
 	}
-
+	parseVars();
 	var indentVal=$('input:radio[name=indent]:checked').val();
 	if (indentVal=='indentt') indentS='\t';
 	else if (indentVal=='indent2') indentS='  ';
 	else if (indentVal=='indent4') indentS='    ';
 
+	keepOutsideComments = $('[name=outside]').prop('checked');
+	keepInsideComments = $('[name=inside]').prop('checked');
+	removeHacks = $('[name=removeHacks]').prop('checked');
+
 	$('#output').val(parseCSS($('#input').val()));
 }
+
+function makeMark (num) {
+	var str="";
+	str += '\t\t\t\t\t\t\t\t\t\t\t';
+	while (num--){
+		str += ' '
+	}
+	str += '\t\t\t\t\t\t\t\t\t\t\t';
+	return str
+}
+
+function decodeMark (str) {
+	var num = 0;
+	str.replace(/ /g, function () {
+		num ++
+	})
+	return num
+}
+
 function parseCSS(s) {
 	var least={children:{}};
 
-	// Remove comments
-	s=s.replace(/\/\*[\s\S]*?\*\//gm, '');
+
+	comments = {};
+	var i = 1;
+
+	//replace comments with marks;
+	s=s.replace(/\/\*[\s\S]*?\*\//gm, function (a){
+		comments[++i] = a;
+		return makeMark(i);
+	});
+
+	//inside comments to fake declarations;
+	s=s.replace(/([^{]+)\{([^}]+)\}/g, function(group, selector, declarations) {
+
+		return selector + '{' +declarations.replace(/\t{10} +\t{10}/g, function (a){
+			a = decodeMark(a);
+			return 'comment__-'+a+': '+a+';'
+		})+'}';
+	});
+
+	//outside comments to fake selectors;
+	s=s.replace(/\t{10} +\t{10}/g, function (a){
+		a = decodeMark(a);
+		return '.__comment__-'+a+' { index: '+a+';}'
+	});
 
 	s.replace(/([^{]+)\{([^}]+)\}/g, function(group, selector, declarations) {
 		var o={};
@@ -98,7 +168,30 @@ function parseCSS(s) {
 			}
 		}
 
+
 		declarations.replace(/([^:;]+):([^;]+)/g, function(decl, prop, val) {
+
+			//remove hacks
+			if(removeHacks){
+				decl = decl
+				.replace(/\s*[\*_].*/g, '')
+				.replace(/.*(-webkit-|-o-|-moz-|-ms-|-khtml-|DXImageTransform|\\9|\\0|expression|opacity\s*=).*/g, '')
+				if (!$.trim(decl)) {
+					return
+				}
+			};
+
+			val = trimColor(val);
+			if (vars[val]){
+				val = vars[val];
+			} else {
+				var a = [];
+				$.each(val.split(' '), function(){
+					a.push(vars[this] || this)
+				});
+				val = a.join(' ');
+			}
+			
 			var declaration={
 				source:decl,
 				property:tr(prop),
@@ -107,6 +200,7 @@ function parseCSS(s) {
 			path.declarations.push(declaration);
 		});
 	});
+
 	return exportObject(least);
 }
 var depth=0;
@@ -114,11 +208,21 @@ var s='';
 function exportObject(path) {
 	var s='';
 	$.each(path.children, function(key, val) {
-		s+=getIndent()+key+' '+openingBracket+'\n';
+		if(key.slice(0,12) == '.__comment__'){
+			keepOutsideComments && (s+=getIndent() + comments[val.declarations[0].value]+'\n');
+			return
+		} else {
+			s+=getIndent()+key+' '+openingBracket+'\n';
+		}
 		depth++;
 		for (var i=0; i<val.declarations.length; i++) {
 			var decl=val.declarations[i];
-			s+=getIndent()+decl.property+semiColumn+' '+decl.value+eol+'\n';
+			if (decl.property.slice(0,9) == 'comment__'){
+				keepInsideComments && (s+=getIndent() + comments[decl.value]+'\n');
+			} else {
+				s+=getIndent()+decl.property+semiColumn+' '+decl.value+eol+'\n';
+			}
+			
 		}
 		s+=exportObject(val);
 		depth--;
